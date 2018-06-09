5cd2549d1fab492bf5c72a0edca61af89ea30706
Xavier Via <fernando.via@gmail.com>
2018-06-09 18:40:15 +0200

    Beginning extraction

---
+++ src/html/Kbd.js
@@ -0,0 +1,6 @@
+/* Generated code, from '/scripts/generatePrimitives.js' */
+// Don't do manual modifications, they will be overriden
+import React from 'react'
+import ReactDream from 'react-dream'
+
+export default ReactDream(props => <kbd {...props} />).name('Kbd')
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import * as _Html from './primitives/html'
+import * as _Svg from './primitives/svg'
+
+export const Html = _Html
+export const Svg = _Svg
